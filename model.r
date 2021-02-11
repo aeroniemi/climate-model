0c9c49a70179f50753f11a28f2465b06d67fa3e2
Alex Beavil <alex@aeroniemi.com>
2021-02-11 16:17:27 +0000

    tidier docs, removed output properly

--- model.r
+++ model.r
@@ -12,23 +12,23 @@ source("config.r")
 # ------------------------------------------------------------------------------
 for (key in 2:CONFIG$runYears) {
   # ----------------------------------------------------------------------------
-  # Advance core params
+  #* Advance core params
   # ----------------------------------------------------------------------------
-  # advance the year
+  # Advance the year
   TS$year[key] = TS$year[key - 1] + 1
-  # advance CO2
+  # Advance CO2
   TS$co2[key] = calculateCo2(TS$co2[key - 1])
-  # advance albedo
+  # Advance albedo
   TS$albedo[key] = calculateAlbedo(TS$earTemp[key - 1], TS$earTemp[key - 2], TS$albedo[key - 1])
   # ------------------------------------------------------------------------------
-  # Calculate sub-temperatures
+  #* Calculate sub-temperatures
   # ------------------------------------------------------------------------------
-  # calculate new albedo temp
+  # Calculate new albedo temp
   TS$albTemp[key] = calculateAlbedoTemp(TS$albedo[key])
-  # calculate new CO2 temp
+  # Calculate new CO2 temp
   TS$co2Temp[key] = calculateCo2Temp(TS$co2[key])
   # ----------------------------------------------------------------------------
-  # Calculate final temperature
+  #* Calculate final temperature
   # ----------------------------------------------------------------------------
   TS$earTemp[key] = TS$albTemp[key] + TS$co2Temp[key]
 }
